9586a7b872123e98a3b82fe8a83609afe93fffb8
Astraea <etebam@proton.me>
2025-05-11 12:44:49 +0100

    pushing config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/eefp/', // Replace 'eefp' with your repository name
 })
