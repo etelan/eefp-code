8c57afe4423c3ae93a0d303c190aa34058684ffe
Astraea <etebam@proton.me>
2025-05-11 20:42:15 +0100

    Final Push Before Launch

--- src/defaultPage/DefaultPage.jsx
+++ src/defaultPage/DefaultPage.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DefaultPage.css";
 import BPPipelineVisualisation from "../bppipeline/BPPipelineVisualisation";
 
 export default function DefaultPage({ children }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="page-container">
-      <nav className="navbar">
-        {[
-          { label: "Home", url: "/" },
-          { label: "Our Demands", url: "/#/our-demands" },
-          { label: "The Problem", url: "/#/the-problem" },
-          { label: "The Journey So Far", url: "/#/the-journey-so-far" },
-          { label: "News", url: "/#/news" },
-          { label: "Donate", url: "https://opencollective.com/energy-embargo" },
-        ].map((item) => (
-          <a key={item.label} href={item.url} className="nav-link">
-            {item.label}
-          </a>
-        ))}
+      <button className="toggle-button" onClick={toggleDropdown}>
+        ☰ {/* Hamburger menu icon */}
+      </button>
+
+      <nav className={`navbar`}>
+        
+        <div className={`nav-links ${isDropdownOpen ? "active" : ""}`}>
+          {[
+            { label: "Home", url: "/" },
+            { label: "Our Demands", url: "/#/our-demands" },
+            { label: "The Problem", url: "/#/the-problem" },
+            { label: "The Journey So Far", url: "/#/the-journey-so-far" },
+            { label: "News", url: "/#/news" },
+            { label: "Donate", url: "https://opencollective.com/energy-embargo" },
+          ].map((item) => (
+            <a key={item.label} href={item.url} className="nav-link">
+              {item.label}
+            </a>
+          ))}
+        </div>
       </nav>
 
       {/* Social Navbar */}
@@ -51,7 +64,7 @@ export default function DefaultPage({ children }) {
             Take Action!
           </a>
 
-          <a href="/news" className="button">
+          <a href="/#/news" className="button">
             Read our news and updates
           </a>
       </div>
