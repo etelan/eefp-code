c840c4abe51e5f36384327f6892b70314217a230
Astraea <etebam@proton.me>
2025-05-11 17:47:24 +0100

    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eefp/', // Replace 'eefp' with your repository name
+  base: 'https://github.com/etelan/eefp-code/',
 })
